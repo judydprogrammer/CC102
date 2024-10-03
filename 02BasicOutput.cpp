9373b7e3251c4963a0dfa92140fc13482f69fdcb
judy balderama <judybldrm@gmail.com>
2024-10-03 09:22:27 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+
+using namespace std;
+
+int main ()
+{
+	cout << "Our Lady of Lourdes College Foundation" << endl;
+	cout << "College of Computer Studies" << endl;
+	cout << "BSIT 1-A" << endl;
+	cout << "Judy Ann F. Balderama" << endl;
+	cout << "Purok 1 Brgy. Mandazo, Basud Camarins Norte" << endl;
+	return 0;
+}
